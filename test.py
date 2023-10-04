12f0f0f700d8dc7b7e24f84b1c71be9e549c2e38
Hongchang Kuang <kuang5005@163.com>
2023-10-04 16:55:54 -0700

    test.py added

---
+++ test.py
@@ -0,0 +1,7 @@
+
+if __name__ == "__main__":
+    x = input(f"Which courses are you taking?\n")
+    if x == "ECE 180":
+        print(x + " is the best class ever")
+    else:
+        print("Bruins are the best!")

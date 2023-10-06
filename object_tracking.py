7bb94ff387445a2380ae7576398881b027586e16
Hongchang Kuang <kuang5005@163.com>
2023-10-05 18:50:14 -0700

    object locating revised

--- object_tracking.py
+++ object_tracking.py
@@ -15,25 +15,32 @@ while(1):
     # Convert BGR to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     # define range of blue color in HSV
-    lower_blue = np.array([110,50,50])
-    upper_blue = np.array([130,255,255])
+    # lower_blue = np.array([110,50,50])
+    # upper_blue = np.array([130,255,255])
+
+    lower_blue = np.array([0,0,150])
+    upper_blue = np.array([120,120,255])
     # Threshold the HSV image to get only blue colors
     mask = cv.inRange(hsv, lower_blue, upper_blue)
     # Bitwise-AND mask and original image
     res = cv.bitwise_and(frame,frame, mask= mask)
 
     # These codes are added by myself
-    # draw the localization box using contor
-    ret,thresh = cv.threshold(frame,127,255,0)
+    # draw the localization box of white object using contor
+    gray_frame = cv.cvtColor(hsv, cv.COLOR_RGB2GRAY)
+    ret,thresh = cv.threshold(gray_frame,127,255,0)
     contours,hierarchy = cv.findContours(thresh, 1, 2)
-    cnt = contours[0]
-
-    rect = cv.minAreaRect(cnt)
-    box = cv.boxPoints(rect)
-    box = np.int0(box)
-    cv.drawContours(frame,[box],0,(0,0,255),2)
+    
+    try:
+        cnt = contours[0]
+    except:
+        pass
+    else:
+        x,y,w,h = cv.boundingRect(cnt)
+        cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
     # These codes are added by myself
 
+    
     cv.imshow('frame',frame)
     # cv.imshow('mask',mask)
     # cv.imshow('res',res)
